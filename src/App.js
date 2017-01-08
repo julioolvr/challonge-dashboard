e7426f8f786de4dea03bc2a27fdc6d635309c764
Julio Olivera <julio.olvr@gmail.com>
2017-01-08 12:55:01 -0300

    Basic tournament fetching

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+function tournamentKeyFromUrl(url) {
+  const [, org, tournamentName] = url.match(/(?:https?:\/\/)?(?:(\w+?)\.)?challonge\.com\/(\w+)/)
+  return org ? `${org}-${tournamentName}` : tournamentName
+}
 
 class App extends Component {
+  onTournamentUrlChange(e) {
+    this.setState({ tournamentUrl: e.target.value })
+  }
+
+  onApiKeyChange(e) {
+    this.setState({ apiKey: e.target.value })
+  }
+
+  onFetchClick() {
+    const { tournamentUrl, apiKey } = this.state
+    const tournamentKey = tournamentKeyFromUrl(tournamentUrl)
+
+    fetch(`/tournaments/${tournamentKey}.json?api_key=${apiKey}`)
+      .then(r => r.json())
+      .then(response => response.tournament)
+      .then(tournament => this.setState({ tournament }))
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <label htmlFor="tournament-url">Tournament URL:</label>
+        <input id="tournament-url" type="text" onChange={ e => this.onTournamentUrlChange(e) } />
+        <label htmlFor="api-key">API Key:</label>
+        <input id="api-key" type="password" onChange={ e => this.onApiKeyChange(e) } />
+        <button onClick={ () => this.onFetchClick() }>Fetch</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
