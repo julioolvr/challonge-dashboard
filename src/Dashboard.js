17da1d750949e511b99aff365ead05d22de7d523
Julio Olivera <julio.olvr@gmail.com>
2017-01-26 01:34:14 -0300

    Revert "Add Table chart" (#7)

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,44 +1,23 @@
 import React, { PropTypes } from 'react'
 import { GridList, GridTile } from 'material-ui/GridList'
 import MaxPoints from './charts/MaxPoints'
-import Table from './charts/Table'
-import Spinner from './components/Spinner';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
 
 const Dashboard = props => {
-  if (!props.tournamentId) {
+  if (!props.selectedTournament) {
     return <div style={{textAlign: 'center', marginTop: '10px'}}>Select a tournament 👆</div>
   }
-  if (props.data.loading) return <Spinner />
 
   return (
-    <div style={{ width: '95%', margin: '0 auto' }}>
-      <Table tournament={props.data.tournament} />
-      <GridList cellHeight={400} style={{ marginTop: '30px' }}>
-        <GridTile>
-          <MaxPoints tournament={props.data.tournament} />
-        </GridTile>
-      </GridList>
-    </div>
+    <GridList cellHeight={360}>
+      <GridTile>
+        <MaxPoints tournamentId={props.selectedTournament}/>
+      </GridTile>
+    </GridList>
   )
 }
 
-const DashboardQuery = gql`
-  query DashboardQuery($tournamentId: ID!) {
-    tournament(id: $tournamentId) {
-      ...Table
-      ...MaxPoints
-    }
-  }
-  ${Table.fragments.tournament}
-  ${MaxPoints.fragments.tournament}
-`
-
 Dashboard.propTypes = {
-  tournamentId: PropTypes.string
+  selectedTournament: PropTypes.number
 }
 
-export default graphql(DashboardQuery, {
-  skip: (ownProps) => !ownProps.tournamentId,
-})(Dashboard)
+export default Dashboard
